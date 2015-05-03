67b0245239b15c9f0d00e3104797a70dacc68138
Richard Grundy <richard@flomio.com>
2015-05-03 07:29:09 -0400

    Adds the beacon detection from the AppDelegate for the Blackdove beacon UUID

--- Sili/Sili/AppDelegate.h
+++ Sili/Sili/AppDelegate.h
@@ -10,14 +10,16 @@
 #import <CoreData/CoreData.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, CLLocationManagerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 - (void)saveContext;
 - (NSURL *)applicationDocumentsDirectory;
